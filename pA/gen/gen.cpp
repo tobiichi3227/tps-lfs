f9cd06e5312e1d7af6dbfa8d930e8de6a66b93ac
tobiichi3227 <cz1346219@gmail.com>
2025-10-29 10:03:39 +0800

    test lfs

--- pA/gen/gen.cpp
+++ pA/gen/gen.cpp
@@ -6,9 +6,9 @@ int main(int argc, char* argv[]) {
 	registerGen(argc, argv, 1);
 	// about testlib, see https://codeforces.com/blog/entry/18291
 
-	// TODO
-	int n = atoi(argv[1]);
-	cout << n << endl;
+    for (int i = 0; i < 1110 * 1110; i++) {
+        cout << rnd.next(1110, 1110 * 1110) << endl;
+    }
 
 	return 0;
 }
